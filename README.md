d97d3e29bf195436b62f6991704bf5343c854b3a
randomtoor <rrbarbosa@gmail.com>
2015-09-03 22:23:12 +0200

    completed first part. Surface and contour graphs could use some extra work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ml_ang_in_r
+
 Implementing the exercises of Andrew Ng's Machine Learning course in R
+
+[Course website](https://www.coursera.org/learn/machine-learning/).
+
+
--- ex1/ex1.R
+++ ex1/ex1.R
@@ -64,7 +64,6 @@ iterations = 1500
 alpha = 0.01
 
 ## compute and display initial cost
-#print(computeCost(X, y, theta))
 #
 ## run gradient descent
 res <- gradientDescent(X, y, theta, alpha, iterations)
@@ -80,67 +79,70 @@ temp <- cbind(data, X%*%theta) #add regression y values
 df <- as.data.frame(temp)
 colnames(df) <- c("X","y","pred")
 ## ggplot2 way
-#require(ggplot2)
-#require(reshape2)
-#df <- melt(df, id.var="X")
-#p2 <- ggplot(df, aes(x=X, y=value)) 
-#p2 <- p2 + geom_point(data=df[df$variable=='y',], aes(colour=variable), shape='x', size=5)
-#p2 <- p2 + geom_line(data=df[df$variable=='y']df[df$variable=='pred',], aes(colour=variable) )
-#p2 <- p2 + ylab('Profit in $10,100s')
-#p2 <- p2 + xlab('Population of City in 10,000s')
-#print(p2)
-#ggsave(p2, file="/Users/barbosarr/Desktop/ratings.pdf", scale=2)
+require(ggplot2)
+require(reshape2)
+df <- melt(df, id.var="X")
+p2 <- ggplot(df, aes(x=X, y=value)) 
+p2 <- p2 + geom_point(data=df[df$variable=='y',], aes(colour=variable), shape='x', size=5)
+p2 <- p2 + geom_line(data=df[df$variable=='pred',], aes(colour=variable) )
+p2 <- p2 + ylab('Profit in $10,100s')
+p2 <- p2 + xlab('Population of City in 10,000s')
+print(p2)
+#ggsave(p2, file="~/Desktop/ratings.pdf", scale=2)
 
 ## standard way
-plot(df$X, df$y,pch=3,col='red', xlab="population", ylab="profit")
-lines(df$X, df$pred, col='blue')
-legend("bottomright",'',c("y","pred"))
-
+#plot(df$X, df$y,pch=3,col='red', xlab="population", ylab="profit")
+#lines(df$X, df$pred, col='blue')
+#legend("bottomright",'',c("y","pred"))
 #legend('Training data', 'Linear regression')
-#hold off % don't overlay any more plots on this figure
-#
+
 ## Predict values for population sizes of 35,000 and 70,000
-#predict1 = [1, 3.5] *theta;
-#fprintf('For population = 35,000, we predict a profit of %f\n',...
-#    predict1*10000);
-#predict2 = [1, 7] * theta;
-#fprintf('For population = 70,000, we predict a profit of %f\n',...
-#    predict2*10000);
-#
-#fprintf('Program paused. Press enter to continue.\n');
-#pause;
+predict1 = c(1, 3.5) %*% theta;
+cat('For population = 35,000, we predict a profit of ' , predict1*10000 , '\n');
+predict2 = c(1, 7) %*% theta;
+cat('For population = 70,000, we predict a profit of ' , predict2*10000 , '\n');
+readline("Press <return to continue") 
+
 #
 ##% ============= Part 4: Visualizing J(theta_0, theta_1) =============
-#fprintf('Visualizing J(theta_0, theta_1) ...\n')
+cat('Visualizing J(theta_0, theta_1) ...\n')
 #
 ## Grid over which we will calculate J
-#theta0_vals = linspace(-10, 10, 100);
-#theta1_vals = linspace(-1, 4, 100);
+theta0_vals = seq(-10, 10, length=100);
+theta1_vals = seq(-1, 4, length=100);
 #
 ## initialize J_vals to a matrix of 0's
-#J_vals = zeros(length(theta0_vals), length(theta1_vals));
-#
+J_vals = matrix(0, length(theta0_vals), length(theta0_vals));
+
 ## Fill out J_vals
-#for i = 1:length(theta0_vals)
-#    for j = 1:length(theta1_vals)
-#	  t = [theta0_vals(i); theta1_vals(j)];    
-#	  J_vals(i,j) = computeCost(X, y, t);
-#    end
-#end
-#
-#
-## Because of the way meshgrids work in the surf command, we need to 
-## transpose J_vals before calling surf, or else the axes will be flipped
-#J_vals = J_vals';
-## Surface plot
-#figure;
-#surf(theta0_vals, theta1_vals, J_vals)
-#xlabel('\theta_0'); ylabel('\theta_1');
-#
+source("computeCost.R")
+for (i in 1:length(theta0_vals)) {
+	for (j in 1:length(theta1_vals)) {
+		 t = c(theta0_vals[i], theta1_vals[j]);
+		 J_vals[i, j] = computeCost(X, y, t);
+	}
+}
+
+
+# Not as pretty as matlab's surf, but does the job
+# TODO: colors!
+persp(theta0_vals, theta1_vals, J_vals,
+	theta=30, phi=30, expand=0.6,
+	col='lightblue', shade=0.75, ltheta=120,
+	ticktype='detailed');
+
+
 ## Contour plot
-#figure;
-## Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-#contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-#xlabel('\theta_0'); ylabel('\theta_1');
-#hold on;
-#plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+# TODO: needs some work with data frame creation and melt function. There's gotta be a better way...
+
+df = data.frame(theta0_vals, theta1_vals);
+df$J_vals = J_vals;
+melted = melt(df$J_vals); # not very sure what this does yet
+ax = expand.grid(theta0_vals, theta1_vals);
+melted$x = ax[,1];
+melted$y = ax[,2];
+names(melted) <- c("a", "b", "z", "x", "y")
+plt <- ggplot(melted, aes(x, y, z = log(z)));
+##plt <- plt + geom_tile(aes(fill=Jval));# + scale_fill_gradientn(colours=matlab.like(10));
+plt <- plt + stat_contour(aes(colour = ..level..));
+print(plt);
